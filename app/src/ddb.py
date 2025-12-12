4dbfc1eab152fa3d4fb135c358ce71beab90ff69
siadgit <siadadam2@gmail.com>
2025-12-12 16:45:16 +0000

    add: dockerfile & ui

--- app/src/ddb.py
+++ app/src/ddb.py
@@ -1,11 +1,11 @@
-import os, boto3
+# import os, boto3
 
 # TABLE_NAME must be provided via ECS task environment
-_table = boto3.resource("dynamodb").Table(os.environ["TABLE_NAME"])
+# _table = boto3.resource("dynamodb", region_name=os.environ["AWS_REGION"]).Table(os.environ["TABLE_NAME"])
 
-def put_mapping(short_id: str, url: str):
-    _table.put_item(Item={"id": short_id, "url": url})
+# def put_mapping(short_id: str, url: str):
+#     _table.put_item(Item={"id": short_id, "url": url})
 
-def get_mapping(short_id: str):
-    resp = _table.get_item(Key={"id": short_id})
-    return resp.get("Item")
+# def get_mapping(short_id: str):
+#     resp = _table.get_item(Key={"id": short_id})
+#     return resp.get("Item")
