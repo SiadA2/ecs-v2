ca00a6d3fc6560e65b7cc7c80dc7ea4d93f2aa8a
siadgit <siadadam2@gmail.com>
2025-12-21 08:27:06 +0000

    rm: comment hashtag on line 4

--- app/src/app.py
+++ app/src/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 import os, hashlib, time
-# from .ddb import put_mapping, get_mapping
+from .ddb import put_mapping, get_mapping
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, FileResponse
 from pathlib import Path
