17e3fcede2871759fa7a1b85fede2d6ae207a2c8
siadgit <siadadam2@gmail.com>
2025-12-25 11:03:08 +0000

    add: whole sec-grps config

--- terraform/prod/modules/security-grps/outputs.tf
+++ terraform/prod/modules/security-grps/outputs.tf
@@ -5,3 +5,7 @@ output "alb_security_group" {
 output "ecs_security_group" {
   value = aws_security_group.ecs_tasks.id
 }
+
+output "vpc_endpoint_security_group" {
+  value = aws_security_group.vpc_endpoint.id
+}
